3db0a2b90811731166f2cdecc6860c9379145c31
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-02-20 15:22:53 +1300

    Don't support EOL rubies.

--- lib/trenni/sanitize.rb
+++ lib/trenni/sanitize.rb
@@ -18,8 +18,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require_relative 'sanitize/extensions'
-
 require_relative 'sanitize/text'
 require_relative 'sanitize/fragment'
-
